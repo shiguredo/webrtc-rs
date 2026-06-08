a233c5e7d1d7e848cd3d4aae2fa316617d974c6d
melpon <shigemasa7watanabe+github@gmail.com>
2026-06-08 17:46:25 +0900

    0055: peer_connection_interface.h のコメントアウト済み旧仮想関数を削除する

--- webrtc/src/webrtc_c/api/peer_connection_interface.h
+++ webrtc/src/webrtc_c/api/peer_connection_interface.h
@@ -289,12 +289,6 @@ webrtc_PeerConnectionInterface_RTCOfferAnswerOptions_set_use_obsolete_sctp_sdp(
 // -------------------------
 
 struct webrtc_PeerConnectionObserver_cbs {
-  // void OnSignalingChange(
-  //     webrtc::PeerConnectionInterface::SignalingState new_state) override {
-  //   RTC_LOG(LS_INFO) << "OnSignalingChange: new_state="
-  //                    << webrtc::PeerConnectionInterface::AsString(new_state);
-  // }
-  // void OnDataChannel(webrtc::scoped_refptr<webrtc::DataChannelInterface> data_channel) override {}
   void (*OnStandardizedIceConnectionChange)(
       webrtc_PeerConnectionInterface_IceConnectionState new_state,
       void* user_data);
@@ -323,10 +317,6 @@ struct webrtc_PeerConnectionObserver_cbs {
   void (*OnIceGatheringChange)(
       webrtc_PeerConnectionInterface_IceGatheringState new_state,
       void* user_data);
-  // void OnIceCandidate(const webrtc::IceCandidate* candidate) override {}
-  // void OnIceCandidateError(const std::string& address, int port, const std::string& url, int error_code, const std::string& error_text) override {}
-  // void OnTrack(webrtc::scoped_refptr<webrtc::RtpTransceiverInterface> transceiver) override {}
-  // void OnRemoveTrack(webrtc::scoped_refptr<webrtc::RtpReceiverInterface> receiver) override {}
 };
 WEBRTC_EXPORT struct webrtc_PeerConnectionObserver*
 webrtc_PeerConnectionObserver_new(
