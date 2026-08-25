c86ba3bdd0e3a3a4fa2492891a325783ba537bd5
melpon <shigemasa7watanabe+github@gmail.com>
2026-08-26 07:56:08 +0900

    0078 ScopedRef 系の内部機構を pub(crate) 化する
    - ScopedRef / RefCountedHandle を pub(crate) 化する
    - from_refcounted_ptr も pub(crate) 化する
    - 公開 API から ScopedRef を外す

--- src/lib.rs
+++ src/lib.rs
@@ -26,7 +26,7 @@ pub use libyuv::{
     i420_rotate, i420_to_nv12, mjpg_size, mjpg_to_i420, mjpg_to_nv12, nv12_copy, nv12_to_i420,
     yuy2_to_i420,
 };
-pub use ref_count::{RefCountedHandle, ScopedRef};
+use ref_count::ScopedRef;
 pub use rtc_base::{
     SSLCertChainRef, SSLCertificateRef, SSLCertificateVerifier, SSLCertificateVerifierHandler,
     SSLIdentity, Thread, TimestampAligner, log, random_bytes, random_string, rtc_log_format_file,
